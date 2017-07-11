77626143a033452377cd6c80ab7cf9355fb2829e
direded <dmpalyonniy@gmail.com>
2017-07-11 12:30:13 +1000

    Add resource loader file

--- main.js
+++ main.js
@@ -20,8 +20,3 @@ function test_func(){
 	window.onkeydown = plr1HandleInput.isKeyDown;
 	window.onkeyup = plr1HandleInput.isKeyUp;
 }
-
-resources.img.load("src/bullet.png", "bullet");
-resources.img.load("src/ply_model.png", "p_ship_1");
-resources.img.onReady(game.start);
-resources.img.onReady(test_func);
---
+++ resources.js
@@ -0,0 +1,4 @@
+resources.img.load("src/bullet.png", "bullet");
+resources.img.load("src/ply_model.png", "p_ship_1");
+resources.img.onReady(game.start);
+resources.img.onReady(test_func);
