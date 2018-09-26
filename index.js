9cff35f09f8a705ae03118b433735f7c2830f7df
direded <dmpalyonniy@gmail.com>
2018-09-26 14:34:14 +0300

    Redesign

---
+++ index.js
@@ -0,0 +1,24 @@
+// misc
+
+let textSize = function(el, fontSize) {
+	el.style.fontSize = fontSize
+	var height = (el.offsetHeight + 1)
+	var width = (el.offsetWidth + 1)
+	return [width, height]
+}
+
+let link = [
+	{
+		caption: 'FEFU Invaders',
+	},
+	{
+		caption: 'WebGL testing'
+	}
+]
+
+let fontSize = 18
+let id = 0
+
+window.onload = function() {
+
+}
